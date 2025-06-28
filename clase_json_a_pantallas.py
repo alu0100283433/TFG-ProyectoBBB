6bc5f75ca7dacb1ba9da3f0f5212b48e466db4f2
Manuel Ramon Regalado Peraza <alu0100283433@ull.edu.es>
2025-06-28 21:28:24 +0000

    Limpia clase_json_a_pantallas.py

--- clase_json_a_pantallas.py
+++ clase_json_a_pantallas.py
@@ -1,10 +1,14 @@
 
+# ******************************************************************************
 # Nombre: clase_json_a_pantallas.py
 #
-#   Descripcion: Abre y carga el fichero JSON con los datos de
-# las pantallas, convierte estos en objetos de tipo pantalla
-# y los guarda en una lista de las mismas para ser enviadas
-# facilmente a otro sitios (otros objetos, por ejemplo)
+#   Descripcion: 
+#
+#       * Abre y carga el fichero JSON con los datos de las pantallas,
+# convierte estos en objetos de tipo pantalla y los guarda en una lista
+# de las mismas para ser enviadas facilmente a otro sitios (otros objetos,
+# por ejemplo)
+# ******************************************************************************
 
 import pdb
 import json
@@ -13,6 +17,7 @@ import clase_pantalla
 import utilidades
 
 
+#.....................
 class Json_a_pantallas:
 
     # =========================================================================
@@ -23,6 +28,7 @@ class Json_a_pantallas:
     # =========================================================================
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Constructor
     def __init__(self, json_a_abrir):
 
         self.fichero_a_abrir = json_a_abrir
@@ -37,6 +43,7 @@ class Json_a_pantallas:
         self.conversion_a_pantallas()
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Abre el Json de pantallas de comandos o el del resto de pantallas.
     def abrir_leer_json(self):
 
         if self.fichero_a_abrir == "pantallas":
@@ -48,20 +55,9 @@ class Json_a_pantallas:
             self.lista_datos_json = json.load(fj)
 
 
-        """
-        # Pantallas normales
-        with open(self.RUTA_JSON, 'r') as fichero_json:
-            self.lista_datos_json = json.load(fichero_json)
-
-        # Pantallas comandos 
-        with open(self.RUTA_COMANDOS_JSON, 'r') as fichero_json:
-            self.lista_datos_comandos_json = json.load(fichero_json)
-        """
-
-
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #   Ajusta el titulo de las pantallas principales para que ponga el
     # numero de pagina y las totales.
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def ajustar_ppales(self):
         p_ppales_totales = 0
         tam_linea = 20
@@ -91,12 +87,10 @@ class Json_a_pantallas:
 
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Guarda en una lista de objetos pantalla datos de cada pantalla del Json
     def conversion_a_pantallas(self):
 
-        #lista_total = self.lista_datos_json + self.lista_datos_comandos_json
-
         for json_p in self.lista_datos_json:
-        #for json_p in lista_total:
             aux_p = clase_pantalla.Pantalla()
 
             aux_p.set_codigo(tuple(json_p["codigo_p"]))
@@ -110,19 +104,6 @@ class Json_a_pantallas:
             aux_p.set_pos_validas(tuple(json_p["posiciones_validas"]))
             aux_p.set_pos_actual_cursor(json_p["posicion_cursor_actual"])
 
-            #breakpoint()
-            """
-            if (type(json_p["linea1"])) == str:
-                aux_p.set_linea1(json_p["linea1"])
-                aux_p.set_linea2(json_p["linea2"])
-                aux_p.set_linea3(json_p["linea3"])
-                aux_p.set_linea4(json_p["linea4"])
-            else:
-                aux_p.set_linea1(json_p["linea1"][0])
-                aux_p.set_linea2(json_p["linea2"][0])
-                aux_p.set_linea3(json_p["linea3"][0])
-                aux_p.set_linea4(json_p["linea4"][0])
-            """
             aux_p.set_tipo(json_p["tipo"])
 
             aux_p.set_linea1(json_p["linea1"])
@@ -137,43 +118,11 @@ class Json_a_pantallas:
 
             self.lista_pantallas.append(aux_p)
 
-            """
-            print(type(aux_p.codigo))
-
-            print(type(aux_p.nombre_codigo))
-            print(type(aux_p.nombre_pantalla))
-
-            print(type(aux_p.linea1))
-            print(type(aux_p.linea2))
-            print(type(aux_p.linea3))
-            print(type(aux_p.linea4))
-
-            print(type(aux_p.pantalla_siguiente))
-            print(type(aux_p.pantalla_anterior))
-            print(type(aux_p.pantalla_nivel_anterior))
-
-            print(type(aux_p.pantalla_opcion1))
-            print(type(aux_p.pantalla_opcion2))
-            print(type(aux_p.pantalla_opcion3))
-            print(type(aux_p.pantalla_opcion4))
-
-            print(type(aux_p.pos_validas))
-            print(type(aux_p.pos_actual_cursor))
-
-            breakpoint()
-            """
-
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def get_lista_pantallas(self):
         copia_lista_pantallas = self.lista_pantallas.copy()
-        #copia_lista_pantallas = list(self.lista_pantallas)
 
         return copia_lista_pantallas
+#.....................
 
-"""
-zzz = Json_a_pantallas("pantallas")
-lp  = zzz.get_lista_pantallas()
-
-breakpoint()
-"""
