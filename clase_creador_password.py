9f3ef93cb81c8ac9d2f05abc09dd286177c66c11
Manuel Ramon Regalado Peraza <alu0100283433@ull.edu.es>
2025-06-28 23:39:33 +0000

    Limpia de clase_creador_password.py

--- clase_creador_password.py
+++ clase_creador_password.py
@@ -1,8 +1,17 @@
 
+# ******************************************************************************
 # Nombre: clase_creador_password.py
 
-#   Descripcion: Va guardando los caracteres que seran la contrasenha
-# a usar y tiene metodos para devolverla y borrarla. 
+# Descripcion:
+#
+#   * Va guardando los caracteres que serán la contraseña a usar y tiene
+# métodos para devolverla y borrarla. 
+#   * La contraseña se crea desde una opción de menú de la aplicación y
+# sólo se almacenará mientras la aplicación esté en ejecución.
+#   * Se uso es mandarse/ejecutarse directamente cuando se necesite (por
+# ejemplo cuando es necesario la contraseña para ejecutar un comando)
+# ******************************************************************************
+
 
 import pdb
 import time
@@ -11,13 +20,25 @@ from tablas_conversion import CODIGO_CAR_KEY_CODE_LOGIN1, \
                               CODIGO_CAR_KEY_CODE_LOGIN2, \
                               CODIGO_CAR_KEY_CODE_LOGIN3
 
+"""
+from tablas_conversion import CODIGO_CAR_KEY_CODE_PSW1, \
+                              CODIGO_CAR_KEY_CODE_PSW2, \
+                              CODIGO_CAR_KEY_CODE_PSW3
+"""
+
+
+
+#.....................
 class Creador_password():
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Constructor
     def __init__(self):
         self.password = ""
         self.devolver_password = False
 
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Convertir posicion de pantalla lcd a la de la tabla de conversion.
-    # --------------------------------------------------------------------------
     def conversion_posicion(self, pos):
         posicion = pos
 
@@ -29,15 +50,16 @@ class Creador_password():
             posicion = posicion - 24
 
         return posicion
-    #   Busca caracter en la tabla de conversion y lo incorpora en
-    # la contrasenha, si el booleano es False.
-    # --------------------------------------------------------------------------
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #   Busca un caracter en la tabla de conversion y lo incorpora en
+    # la contraseña, si el booleano es False.
     def nuevo_caracter(self, pos, cod_pantalla):
         caracter = ""
 
         posicion = self.conversion_posicion(pos)
 
-        print("Posicion generador password es: ", posicion)
 
         if self.devolver_password == False:
             if   cod_pantalla == (1,3,1):
@@ -46,8 +68,6 @@ class Creador_password():
                 caracter = CODIGO_CAR_KEY_CODE_LOGIN2[posicion]
             elif cod_pantalla == (1,3,3):
                 caracter = CODIGO_CAR_KEY_CODE_LOGIN3[posicion]
-
-            #breakpoint()
 
             # Identificar si es ENTER o no.
             if caracter == "€;":
@@ -62,25 +82,26 @@ class Creador_password():
                 self.password = self.password + caracter
 
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Elimina el password.
-    # --------------------------------------------------------------------------
     def borrar_password(self):
         self.password = ""
         self.devolver_password = False
 
 
-    # Envia contrasenha.
-    # --------------------------------------------------------------------------
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Envia la contraseña.
     def get_password(self):
         if self.devolver_password == False:
             return -1
         else:
             return self.password
 
-    # Obtener estado password (se ha pulsado ENTER o no
-    # --------------------------------------------------------------------------
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Obtener el estado del password ( si se ha pulsado ENTER o no)
     def get_password_completo(self):
         return self.devolver_password
+#.....................
 
 
 
